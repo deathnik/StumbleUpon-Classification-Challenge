923905c0ad1e16f57eac7e7e47ed44efcdc7fdc8
deathnik <a.e.nikolaichik@gmail.com>
2013-10-16 09:59:39 +0300

    somewhere 0.78585. single naive bayes

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -3,6 +3,8 @@ import scala.Array
 import scala.collection.immutable.HashMap
 import scala.collection.{mutable, immutable}
 import scalax.io.{Output, Resource}
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 /**
  * User: deathnik
@@ -11,16 +13,28 @@ import scalax.io.{Output, Resource}
  */
 object Main {
   val zero = int2Integer(0)
-  var alchemeryCategorySet =mutable.Set[String]()
-    var wordsRegexpr = "[^a-zA-Z]"
+  var alchemeryCategorySet = mutable.Set[String]()
+  var wordSet = Set[String]("for", "you", "your", "body", "title", "have")
+
+  def splitingFunction(text: String): Array[String] = text.toLowerCase().split("[^a-zA-Z-]").filter(s => s.length > 2)
+
+
   def main(args: Array[String]) {
     val mainData = loadData("../train.tsv")
+    val testDate = loadData("../test.tsv")
     for (line <- mainData) alchemeryCategorySet += line(3)
+    /*for(cat <- alchemeryCategorySet){
+      println(cat)
+      getStatistics(true,line =>line(3)==cat && line(17)=="1"   && line(26) == "1",  line=>line(3)==cat && line(17)=="1" && line(26) == "0", mainData)
+      getStatistics(true,line =>line(3)==cat ,  line=>line(3)==cat && line(17)=="1", testDate)
+
+    }  */
+
     /*for(i <- 4 to 25){
       println(i)
       getAverage(l => l(i).replace("?","-1").toDouble,l => l(3)=="health" && l(26)=="1",l => l(3)=="health" && l(26)=="0", mainData)
     }  */
-    test(mainData)
+    test(mainData, s => true)
 
     val file = new File("result.csv")
     file.delete()
@@ -28,45 +42,47 @@ object Main {
     val weightedSet = train(mainData)
     //dumpSet(weightedSet)
     val output: Output = Resource.fromFile("result.csv")
-    val testDate = loadData("../test.tsv")
     output.write("urlid,label\n")
     for (line <- testDate) {
       output.write(line(1) + "," + classify(line, weightedSet) + "\n")
     }
   }
-  def dumpSet(weightedSet: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])){
+
+  def dumpSet(weightedSet: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])) {
     val dump: Output = Resource.fromFile("../statistics/words");
-    for(w <-weightedSet._1.iterator){
+    for (w <- weightedSet._1.iterator) {
       dump.write("\n")
-      dump.write(w._1+"\n")
-      for(e<- w._2){
-        dump.write(e._1+" "+"\n")
+      dump.write(w._1 + "\n")
+      for (e <- w._2) {
+        dump.write(e._1 + " " + "\n")
       }
     }
     dump.write("\n\n\nneg")
-    for(w <-weightedSet._2.iterator){
+    for (w <- weightedSet._2.iterator) {
       dump.write("\n")
-      dump.write(w._1+"\n")
-      for(e<- w._2){
-        dump.write(e._1+" "+"\n")
+      dump.write(w._1 + "\n")
+      for (e <- w._2) {
+        dump.write(e._1 + " " + "\n")
       }
     }
   }
 
-  def test(mainData: List[Array[String]]) {
-    val splitedData = util.Random.shuffle(mainData).splitAt((mainData.length * 0.7).toInt)
+  def test(mainData: List[Array[String]], testFilter: Array[String] => Boolean) {
+    var splitedData = util.Random.shuffle(mainData).splitAt((mainData.length * 0.7).toInt)
     val weightedSet = train(splitedData._1)
-    for(category <-alchemeryCategorySet) score("Score at "+category+":",splitedData._2.filter(s => s(3) == category),weightedSet)
-    score("Overall score:",splitedData._2,weightedSet)
+    splitedData = (splitedData._1, splitedData._2.filter(s => testFilter(s)))
+    for (category <- alchemeryCategorySet) score("Score at " + category + ":", splitedData._2.filter(s => s(3) == category), weightedSet)
+    score("Overall score:", splitedData._2, weightedSet)
   }
 
-  def score(testName: String, lines : List[Array[String]], weightedSet: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])){
+  def score(testName: String, lines: List[Array[String]], weightedSet: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])) {
     var truePos = 0
     var falsePos = 0
     var trueNeg = 0
     var falseNeg = 0
     for (line <- lines) {
-      if (line(26) == classify(line, weightedSet)) {
+
+      if (line(26) == ( if(classify(line, weightedSet)<0.5) "0" else "1")) {
         if (line(26) == "1") truePos += 1
         else trueNeg += 1
       }
@@ -77,39 +93,43 @@ object Main {
     }
     println()
     println("Count:" + lines.length)
-    println(testName+ 1.0 * (truePos + trueNeg) / (truePos + trueNeg + falseNeg + falsePos))
-    println (trueNeg + " | " + falseNeg)
+    println(testName + 1.0 * (truePos + trueNeg) / (truePos + trueNeg + falseNeg + falsePos))
+    println(trueNeg + " | " + falseNeg)
     println(falsePos + " | " + truePos)
   }
 
-  def getStatistics(show:Boolean,posTest: Array[String]=> Boolean,negTest: Array[String]=> Boolean,data: List[Array[String]] ):Double={
-      var pos = 0
-      var neg = 0
-    for(line<-data){
-      if(posTest(line)) pos +=1
-      else if(negTest(line)) neg+=1
+  def getStatistics(show: Boolean, posTest: Array[String] => Boolean, negTest: Array[String] => Boolean, data: List[Array[String]]): Double = {
+    var pos = 0
+    var neg = 0
+    for (line <- data) {
+      if (posTest(line)) {
+        pos += 1
+      }
+      else if (negTest(line)) {
+        neg += 1
+      }
     }
-    if(show)println("pos:" + pos+ " neg:"+ neg)
-    1.0 * pos/(pos+neg)
+    if (show) println("pos:" + pos + " neg:" + neg)
+    1.0 * pos / (pos + neg)
   }
 
-  def getAverage(grabber: Array[String]=>Double,posTest: Array[String]=> Boolean,negTest: Array[String]=> Boolean,data: List[Array[String]]){
+  def getAverage(grabber: Array[String] => Double, posTest: Array[String] => Boolean, negTest: Array[String] => Boolean, data: List[Array[String]]) {
     var pos = 0.0
     var posCount = 0
     var neg = 0.0
-    var negCount =0
-    for(line<-data){
-      if(posTest(line)){
-        posCount +=1
+    var negCount = 0
+    for (line <- data) {
+      if (posTest(line)) {
+        posCount += 1
         pos += grabber(line)
       }
-      else if(negTest(line)){
-        negCount+=1
-        neg+= grabber(line)
+      else if (negTest(line)) {
+        negCount += 1
+        neg += grabber(line)
       }
     }
-    println("pos avrg:" + (pos/posCount))
-    println("neg avrg:" + (neg/negCount))
+    println("pos avrg:" + (pos / posCount))
+    println("neg avrg:" + (neg / negCount))
   }
 
   def loadData(s: String): List[Array[String]] =
@@ -130,7 +150,7 @@ object Main {
       } else {
         targetMap = negClassif.get(line(3)).get
       }
-      for (word <- line(2).split(wordsRegexpr).filter(s => s.length > 3)) {
+      for (word <- splitingFunction(line(2)).toSet[String]) {
         if (targetMap.get(word) == None) targetMap.+=(word -> 1)
         else targetMap.update(word, targetMap.get(word).get + 1)
       }
@@ -138,27 +158,47 @@ object Main {
     return (posClassif, negClassif)
   }
 
-  def classify(line: Array[String], classify: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])): String = {
+  def classify(line: Array[String], classify: (HashMap[String, mutable.HashMap[String, Int]], HashMap[String, mutable.HashMap[String, Int]])): Double = {
     var scorePos = 0.0
     var scoreNeg = 0.0
+    val words = splitingFunction(line(2))
+    val occur = words.groupBy((word: String) => word).mapValues(_.length)
+    var character = 1.0 * words.size / occur.size
+    /*if(occur.size < 20){
+      println(line(26) + "  " + occur.size+" " + line(2))
+      for(w <- occur){
+        println(w._1 + " "+w._2)
+      }
+    } */
     val catClassify = (classify._1.get(line(3)).get, classify._2.get(line(3)).get)
-    for (word <- line(2).split(wordsRegexpr).filter(s => s.length > 3)) {
+    for (w <- occur) {
+      val word = w._1
       if (catClassify._1.get(word) != None && catClassify._2.get(word) != None) {
-        val div = catClassify._1.get(word).get + catClassify._2.get(word).get  +1
+        val div = catClassify._1.get(word).get + catClassify._2.get(word).get + 1
         scorePos += catClassify._1.get(word).get / div
         scoreNeg += catClassify._2.get(word).get / div
       }
       else if (catClassify._1.get(word) == None && catClassify._2.get(word) == None) {}
       else if (catClassify._1.get(word) == None) {
-        scoreNeg += 1.0 / (catClassify._2.get(word).get +1)
+        scoreNeg += 1.0 / (catClassify._2.get(word).get + 1)
       }
       else {
-        scorePos += 1.0 / (catClassify._1.get(word).get +1)
+        scorePos += 1.0 / (catClassify._1.get(word).get + 1)
       }
 
     }
-    if (scorePos > scoreNeg) return "1"
-    else return "0"
+    return 1.0*(scorePos+1)/(scorePos+scoreNeg+2)
+  }
+
+
+  def cleanAndSplitToArray(text: String): Array[String] = {
+    var json = parse(text.replace("\"\"", "\""))
+    for {
+      JObject(child) <- json
+      JField("title", JString(body)) <- child
+    } return splitingFunction(body)
+    //if no body
+    Array[String]()
   }
 
 }
